1a8bfd141961ec9415c3c208e74887353c9f3172
Cktripa-source <websdeveloper2024@gmail.com>
2024-11-22 17:38:38 +0200

    firstfff

--- includes/footer.php
+++ includes/footer.php
@@ -36,7 +36,7 @@
                 </div>
                 <!-- Legal -->
                 <div>
-                    <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
+                    <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Rules</h2>
                     <ul class="text-gray-500 dark:text-gray-400 font-medium">
                         <li class="mb-4">
                             <a href="#" class="hover:underline">Privacy Policy</a>
